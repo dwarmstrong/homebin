382ad716397b345c2c8f0d75dc098c18f13ed056
Daniel Wayne Armstrong <daniel@circuidipity.com>
2016-07-07 12:29:02 -0400

    first commit

---
+++ rePhoneAndEmail.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# Adapted from "Automate the Boring Stuff with Python / by Al Sweigert"
+'''
+(O< .: rePhoneAndEmail.py - Find and extract phone numbers and email addresses
+(/)_                        on the clipboard
+'''
+
+# Depends ...
+# $ sudo apt install xclip python3-pyperclip
+
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?              # area code
+    (\s|-|\.)?                      # separator
+    (\d{3})                         # first 3 digits
+    (\s|-|\.)                       # separator
+    (\d{4})                         # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+    )''', re.VERBOSE)
+
+emailRegex = re.compile(r'''(
+        [a-zA-Z0-9._%+-]+   # username
+        @                   # @ symbol
+        [a-zA-Z0-9.-]+      # domain name
+        (\.[a-zA-Z]{2,4})   # dot-something
+        )''', re.VERBOSE)
+
+# Find matches in clipboard text
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+# Copy results to the clipboard
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
