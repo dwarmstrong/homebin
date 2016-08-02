a26ac41eaf40304d13f342e2344dd312b088cc6f
Daniel Wayne Armstrong <daniel@circuidipity.com>
2016-08-02 09:08:03 -0400

    logging.disable

--- dateAndWeight.py
+++ dateAndWeight.py
@@ -6,7 +6,7 @@ from dateAndY import (date_and_y, match_date_and_y, gen_list,
 
 logging.basicConfig(level=logging.DEBUG, 
                 format=' %(asctime)s - %(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 logging.debug('Start of program')
 
 msg = '''
