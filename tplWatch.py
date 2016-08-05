1a01682fb58581da7764777a4dd86a3783d619cb
Daniel Wayne Armstrong <daniel@circuidipity.com>
2016-08-05 15:55:06 -0400

    mv watch_for() to webWatch

--- tplWatch.py
+++ tplWatch.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse, logging, bs4, requests
+import argparse, logging
 from os.path import expanduser
+from webWatch import watch_for
 
 logging.basicConfig(level=logging.DEBUG, 
                 format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -21,30 +22,6 @@ home = expanduser("~")
 watchlist = home + "/share/log/tplWatch"
 searchfor = "total-results"
 results = home + "/share/log/tplMatch" 
-
-def watch_for(checklist, element, save_results_to):
-    '''Check a website for a matching ELEMENT from CHECKLIST''' 
-    with open(save_results_to, 'w') as f:
-        f.write('# Items found\n')
-        with open(checklist, 'r') as f_search:
-            for line in f_search:
-                if line.startswith('http'):
-                    logging.debug('{}'.format(line))
-                    f.write(line)
-                    res = requests.get(line)
-                    try:
-                        res.raise_for_status()
-                    except Exception as exc:
-                        logging.debug('{}'.format(exc))
-                        f.write('{}\n'.format(exc))
-                        continue
-                    itemWatch = bs4.BeautifulSoup(res.text, "lxml")
-                    elems = itemWatch.select(element)
-                    if elems[0].getText() != '0':
-                        logging.debug('{}'.format(elems[0]))
-                        f.write('{}\n'.format(elems[0]))
-                    else:
-                        f.write('No match.\n')
 
 if __name__ == '__main__':
     watch_for(watchlist, searchfor, results)
