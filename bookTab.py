c6b0effcc8943f3636257c2df421eb6145ba65ab
Daniel Wayne Armstrong <daniel@circuidipity.com>
2016-08-24 08:54:11 -0400

    main()

--- bookTab.py
+++ bookTab.py
@@ -2,16 +2,20 @@
 # Inspired by "Automate the Boring Stuff with Python" / by Al Sweigart
 from bernersLee import Browser
 
-msg = '''
+msg = """
 (O< .: Get book information from command line arguments or clipboard and open
 (/)_   web browser and search Amazon and the Toronto Public Library.
-'''
-library = "https://www.torontopubliclibrary.ca/search.jsp?Ntt="
-amazonCa = "https://www.amazon.ca/"
-amazonCom = "https://www.amazon.com/"
-amzSearch = "s/ref=nb_sb_noss_1?url=search-alias%3Dstripbooks&field-keywords="
+"""
 
-browser = Browser()
-browser.open_search_tab(library)
-browser.open_search_tab(amazonCa + amzSearch)
-browser.open_search_tab(amazonCom + amzSearch)
+def main():
+    library = "https://www.torontopubliclibrary.ca/search.jsp?Ntt="
+    amazonCa = "https://www.amazon.ca/s/ref=nb_sb_noss_1?"
+    amazonCom = "https://www.amazon.com/s/ref=nb_sb_noss_1?"
+    amzSearch = "url=search-alias%3Dstripbooks&field-keywords="
+
+    browser = Browser()
+    browser.open_search_tab(library)
+    browser.open_search_tab(amazonCa + amzSearch)
+    browser.open_search_tab(amazonCom + amzSearch)
+
+main()
