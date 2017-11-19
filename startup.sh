0200502021cfc2e12bad658c9d0656b78c2e4e2f
Daniel Wayne Armstrong <daniel@circuidipity.com>
2017-11-19 10:25:37 -0500

    first commit

---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# NAME="startup.sh"
+# BLURB="personal startup script"
+# SOURCE="https://github.com/vonbrownie/homebin/blob/master/startup.sh"
+
+# Inspired by "Better Control for Startup Applications"
+# https://ubuntu-mate.community/t/better-control-for-startup-applications/11484
+
+# Play the Ubuntu login sound
+paplay --volume=75000 /usr/share/sounds/ubuntu/stereo/desktop-login.ogg &
+
+sleep 2
+
+# Bind commands to certain keys and load key mappings
+/home/dwa/bin/keyboardconf &
+
+# Clipboard manager
+parcellite &
+
+# Janitor                 
+[ -d ~/.local/share/Trash ] && rm -rf ~/.local/share/Trash/*
