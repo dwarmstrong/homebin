530948aaa26b79a33206d8e9738cdb59f7470cd6
Daniel Wayne Armstrong <daniel@circuidipity.com>
2019-09-16 12:06:49 -0400

    first commit

---
+++ multi_display.py
@@ -0,0 +1,11 @@
+# multi_display.py - Enable/disable extended desktop using XRandR
+
+# Options:
+# * Set PRIMARY and SECONDARY display
+# * Set orientation: left/right; top/bottom
+
+def detect_display():
+    """Detect if a second display is attached."""
+
+def display_layout():
+    """Configure the desktop layout across two displays."""
