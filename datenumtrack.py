5ecdd6af9074e951c6fd492d6955e26dda853846
Daniel Wayne Armstrong <daniel@circuidipity.com>
2019-09-19 12:37:54 -0400

    first commit

---
+++ datenumtrack.py
@@ -0,0 +1,11 @@
+# datenumtrack.py
+#
+# Input:
+#   * start date
+#   * end date
+#   * filename where dates and numbers are recorded
+#   * regex patterns
+#   * column name for numbers (what do the numbers represent?)
+# Output:
+#   * columns for dates and their corresponding numbers
+#   * starting number, end number, average daily change
