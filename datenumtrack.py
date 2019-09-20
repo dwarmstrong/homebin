426baaba5727e655ffe2faac675c2b868023feb1
Daniel Wayne Armstrong <daniel@circuidipity.com>
2019-09-20 19:08:50 -0400

    description

--- datenumtrack.py
+++ datenumtrack.py
@@ -1,11 +1,38 @@
-# datenumtrack.py
+#!/usr/bin/env python3
 #
-# Input:
-#   * start date
-#   * end date
-#   * filename where dates and numbers are recorded
-#   * regex patterns
-#   * column name for numbers (what do the numbers represent?)
-# Output:
-#   * columns for dates and their corresponding numbers
-#   * starting number, end number, average daily change
+# NAME
+#       datenumtrack.py - retrieve dates and corresponding numbers
+#
+# SYNOPSIS
+#       datenumtrack.py [OPTION] FILE
+#
+# DESCRIPTION
+#       FILE contains settings that establish a range of dates along
+#       with their correponding numbers to be retrieved from a
+#       specified file.
+#
+#       Output displays two columns: first column for dates and second
+#       column for the numbers. A bottom line displays starting value,
+#       end value, and average daily change for the calendar period.
+#
+#       Options
+#       -h, --help      this description
+#       -d, --dates     interactive prompt for dates range
+#
+# Source: https://github.com/vonbrownie/homebin/blob/master/datenumtrack.py
+
+# TODO
+
+# Read FILE argument
+
+# Detect if any options specified.
+
+# Set any settings specified by file.
+
+# Use regex patterns to retrieve dates and numbers from source file.
+
+# Display columns of data.
+
+# Display bottom line values.
+
+print("Hello!")
