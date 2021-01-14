780676807aa9b6387bcf401340a16d84749df5f1
Daniel Wayne Armstrong <daniel@dwarmstrong.org>
2021-01-14 14:58:27 -0500

    save

--- lbs2kg.py
+++ lbs2kg.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 #
-# Convert pounds to kilograms.
+# Description: Convert pounds to kilograms.
+# Usage: lbs2kg [number]
 import sys
 
-# Usage: lbs2kg [number]
 pounds = float(sys.argv[1])
 kilograms = pounds * 0.4535924
 print(f"{pounds}lbs = {kilograms:.1f}kg")
